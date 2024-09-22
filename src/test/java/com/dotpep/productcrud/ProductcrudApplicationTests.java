d0e710143e6b75e7448d4660bd0caacc672287dc
dotpep <pepelnine@gmail.com>
2024-09-22 22:07:54 +0500

    spring maven initializr with 1. spring web and 2. spring data mongodb dependencies

---
+++ src/test/java/com/dotpep/productcrud/ProductcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dotpep.productcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
